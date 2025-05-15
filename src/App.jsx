03a61ff0ccb6b62bc73bba56fca07cc9813a2184
Emma <emmalanza@hotmail.com>
2025-05-15 18:55:40 +0200

    config react + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/styles.css';
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hola React + Tailwind</h1>
+    </>
+  )
+}
+
+export default App;
